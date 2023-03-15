dbe6b75d17ab772b6a94cb3b8c29fde5df48d733
Vaibhav Kulkarni <vakulk@iu.edu>
2023-03-15 01:39:20 -0400

    added Create user api without authentication

--- gadgetry_backend/api/views.py
+++ gadgetry_backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
+
+from .models import User
+from .Serializers import UserSerializer
 # Create your views here.
+
+class CreateUserView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+
+createuserView = CreateUserView.as_view()
